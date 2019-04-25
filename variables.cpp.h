8c41298f1def327abb4f1e3600b56a3fc6584a33
farhab9738 <49877696+farhab9738@users.noreply.github.com>
2019-04-25 23:51:33 +0600

    Add files via upload

---
+++ variables.cpp.h
@@ -0,0 +1,52 @@
+#ifndef VARIABLES_H_INCLUDED
+#define VARIABLES_H_INCLUDED
+
+int Ex[100]={0,1300,1700,1550,2100};
+int Ey[100]={0,100,100,250,800};
+
+
+
+int Bx1=0,Bx2=1180,By1=0,By2=0;
+//int Ex[1]=1300;Ex[2]=1700,Ey[1]=200,Ey[2]=380;			//firstexplosions
+//int Ex[3]=1550,Ex[4]=2100,Ey[3]=250,Ey[4]=400;			//secondexplosions	
+
+//struct Explosion Exp[5];
+
+
+int posx = 0;
+int posy = 0;
+int auto_dropy=0;
+int exit1;
+int flag=0;
+int instructions;
+int highscore;
+int helicopter;
+int ground;
+int back1;
+int explosion2;
+int heli1;
+int heli2;
+int heli3;
+int explosion1;
+int explosiontimer;
+//int posy1=340;
+int y;
+//int xforgamescreen=0;
+
+	int i=0;//global_flag
+int passivestart;
+int heliwaytimer;
+int image;
+int start1;
+int gamescreen;
+int flag1=0;
+int flaginstructions=0;
+int flaghighscore=0;
+int flagback=0;
+int flagback1=0;
+int darkclouds;
+int j=0;
+char arr[100];
+
+
+#endif // VARIABLES_H_INCLUDED
